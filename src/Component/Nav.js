15583acae56d0acf8a0630d8a4a1d039440783f6
Kawish786 <kawishshakeel4@gmail.com>
2023-08-17 12:34:00 +0530

    id

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -26,7 +26,7 @@ function Nav() {
     <div className='navbar'>
         <h2 onClick={handleHome}>EdTech</h2>
         <input type='checkbox' id='toggler'/>
-        <AlignJustify htmlFor='toggler' className='label' />
+        <AlignJustify htmlFor='toggler' id='label' />
         <div className='navpart'>
             <ul className='list'>
                 <li onClick={handleHome}><Home /> Home</li>
