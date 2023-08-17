9fb08a4c749930fd187076331b09ad991f2f9970
Kawish786 <kawishshakeel4@gmail.com>
2023-08-17 15:05:53 +0530

    select

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -23,6 +23,101 @@ function Form() {
 
     const handleSelectChange = (event) => {
       setSelectedValue(event.target.value);
+      var courseSelect = document.getElementById("courseSelect");
+      var subCourseSelect = document.getElementById("subCourseSelect");
+      subCourseSelect.innerHTML = "";
+      var selectedCourse = courseSelect.value;
+
+      if (selectedCourse === "Finance") {
+        addSubCourseOption("Financial Accounting");
+        addSubCourseOption("Managerial Accounting");
+        addSubCourseOption("Corporate Finance");
+        addSubCourseOption("Investment Management");
+        addSubCourseOption("Financial Modeling");
+        addSubCourseOption("Behavioral Finance");
+    } else if (selectedCourse === "Entrepreneurship") {
+        addSubCourseOption("Business Plan Development");
+        addSubCourseOption("Startup Financing and Funding");
+        addSubCourseOption("Growth Hacking & Scaling a Startup");
+        addSubCourseOption("Technology and Entrepreneurship");
+        addSubCourseOption("Managing Business Risks");
+        addSubCourseOption("E-commerce and Online Business");
+    } else if (selectedCourse === "Communication") {
+        addSubCourseOption("Public Speaking");
+        addSubCourseOption("Business Communication");
+        addSubCourseOption("Interpersonal Communication");
+        addSubCourseOption("Cross-Cultural Communication");
+        addSubCourseOption("Communication for Leadership");
+        addSubCourseOption("Media and Communication");
+    } else if (selectedCourse === "Management") {
+        addSubCourseOption("Leadership Development");
+        addSubCourseOption("Organizational Behaviour");
+        addSubCourseOption("Marketing Management");
+        addSubCourseOption("Time Management");
+        addSubCourseOption("Crisis Management");
+        addSubCourseOption("Project Management");
+    } else if (selectedCourse === "Sales") {
+        addSubCourseOption("Consultative Selling");
+        addSubCourseOption("Relationship Selling");
+        addSubCourseOption("Sales Negotiation Skills");
+        addSubCourseOption("Solution Selling");
+        addSubCourseOption("Territory Management");
+        addSubCourseOption("Retail Sales Techniques");
+    } else if (selectedCourse === "Strategy") {
+        addSubCourseOption("Business Strategy");
+        addSubCourseOption("Corporate Strategy");
+        addSubCourseOption("Strategic Management");
+        addSubCourseOption("Competitive Strategy");
+        addSubCourseOption("Strategic Planning");
+        addSubCourseOption("Strategic Leadership");
+    } else if (selectedCourse === "Operations") {
+        addSubCourseOption("Operations Management");
+        addSubCourseOption("Logistics Management");
+        addSubCourseOption("Operations Strategy");
+        addSubCourseOption("Operations Analytics");
+        addSubCourseOption("Agile Operations");
+        addSubCourseOption("E-commerce Operations");
+    } else if (selectedCourse === "ProjectManager") {
+        addSubCourseOption("Project Procurement Management");
+        addSubCourseOption("Project Integration Management");
+        addSubCourseOption("Critical Path Method (CPM)");
+        addSubCourseOption("Change Management in Projects");
+        addSubCourseOption("Earned Value Management (EVM)");
+        addSubCourseOption("Risk Assessment and Mitigation");
+    } else if (selectedCourse === "BusinessLaw") {
+        addSubCourseOption("Intellectual Property Law");
+        addSubCourseOption("Securities Regulation");
+        addSubCourseOption("Tax Law and Business");
+        addSubCourseOption("Real Estate Law");
+        addSubCourseOption("Franchise Law");
+        addSubCourseOption("Legal Risk Management");
+    } else if (selectedCourse === "Data&Analytics") {
+        addSubCourseOption("Data Visualization");
+        addSubCourseOption("Statistical Analysis");
+        addSubCourseOption("Data Analysis with R");
+        addSubCourseOption("Data Mining");
+        addSubCourseOption("Big Data Analytics");
+        addSubCourseOption("Business Intelligence");
+    } else if (selectedCourse === "HomeBusiness") {
+        addSubCourseOption("Business Idea Generation");
+        addSubCourseOption("Business Plan Development");
+        addSubCourseOption("Marketing and Branding");
+        addSubCourseOption("Social Media Marketing");
+        addSubCourseOption("Home Business Photography");
+        addSubCourseOption("Dropshipping and E-commerce Fulfillment");
+    } else if (selectedCourse === "HumanResource") {
+        addSubCourseOption("Recruitment and Talent Acquisition");
+        addSubCourseOption("Performance Management");
+        addSubCourseOption("HR Strategy and Planning");
+        addSubCourseOption("HR Metrics and Analytics");
+        addSubCourseOption("HR Technology and Systems");
+        addSubCourseOption("Strategic Staffing");
+    }
+    function addSubCourseOption(subCourse) {
+      var option = document.createElement("option");
+      option.text = subCourse;
+      subCourseSelect.add(option);
+  }
     };
     const handleInputChange=(e)=>{
         const { name, value } = e.target;
@@ -63,7 +158,7 @@ function Form() {
         <input value={inputValues.location} onChange={handleInputChange} className="text" id='location' name='location' type="text"  />
         <label className="label" htmlFor='education'><School /> Highest Education <span className="ast">*</span></label>
         <input value={inputValues.education} onChange={handleInputChange} className="text"  id='education' name='education' type="text"  /> 
-        <select className='select' onChange={handleSelectChange} value={selectedValue} required >
+        <select className='select' id='courseSelect' onChange={handleSelectChange} value={selectedValue} required >
             <option value="">Select your course of study <span className="ast">*</span></option>
             <option value="Finance" >Finance</option>
             <option value="Entrepreneurship" >Entrepreneurship</option>
@@ -78,6 +173,9 @@ function Form() {
             <option value="HomeBusiness" >Home Business</option>
             <option value="HumanResource" >Human Resource</option>
         </select> 
+        <select id="subCourseSelect" className='select'>
+            <option value="">Select a Sub-Course</option>
+        </select>
         </form>
     </div>
     <div>
